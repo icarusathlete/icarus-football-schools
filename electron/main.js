012b6e256cfabace039454cb21ce84c750141ed0
Icarus <athlete@Icaruss-MacBook-Air.local>
2026-04-01 21:11:47 +0530

    Add electron desktop build configuration and workflow

---
+++ electron/main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    title: "Icarus Football Schools",
+    icon: path.join(__dirname, '../dist/favicon.ico'), // Will fallback to default if not found
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    // Premium Look
+    backgroundColor: '#0a1d37', // Matches Icarus 900
+    show: false,
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    // win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
